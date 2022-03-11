035017ce3dc4ed47ff21d357ba0049b9eda43507
loic_hernaut <loic.hernaut@student.vinci.be>
2022-03-11 09:51:09 +0100

    Add BufferReader

--- src/Graph.java
+++ src/Graph.java
@@ -2,47 +2,38 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
 
-public class Graph{
+public class Graph {
+
+    HashSet<Aeroport> ensembleAeroport = new HashSet<Aeroport>();
+    HashSet<Vol> ensembleVol = new HashSet<Vol>();
+
     public Graph(File aeroports, File vols){
 
-    }
-    private static final String FILENAME = "C:\\Users\\quent\\Downloads\\Projet-20220311\\vols.txt";
-    private static final String FILENAME2 = "C:\\Users\\quent\\Downloads\\Projet-20220311\\aeroports.txt";
-
-
-    public static void main(String[] args) {
-
-        BufferedReader bufferedreader = null;
         FileReader filereader = null;
-        FileReader fileReader2 = null;
-        BufferedReader bufferedReader2 = null;
-
+        BufferedReader bufferedReader = null;
 
         try {
 
-            filereader = new FileReader(FILENAME);
-            fileReader2 = new FileReader((FILENAME2));
-            bufferedreader = new BufferedReader(filereader);
-            bufferedReader2= new BufferedReader((fileReader2));
-
+            filereader = new FileReader(aeroports);
+            bufferedReader = new BufferedReader(filereader);
 
             String strCurrentLine;
 
-            while ((strCurrentLine = bufferedreader.readLine()) != null) {
+            while ((strCurrentLine = bufferedReader.readLine()) != null) {
                 System.out.println(strCurrentLine);
             }
 
         } catch (IOException e) {
-
             e.printStackTrace();
-
         } finally {
 
             try {
 
-                if (bufferedreader != null)
-                    bufferedreader.close();
+                if (bufferedReader != null)
+                    bufferedReader.close();
 
                 if (filereader != null)
                     filereader.close();
@@ -51,5 +42,15 @@ public class Graph{
                 e.printStackTrace();
             }
         }
+
     }
+
+    public void calculerItineraireMinimisantNombreVol(String s, String s2) {
+
+    }
+
+    public void calculerItineraireMiniminantDistance(String s, String s2) {
+
+    }
+
 }
